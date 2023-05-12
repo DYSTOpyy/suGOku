2ea36135f91cb7ac82cf1e800509727b4661b6db
DYSTO_pyy <bcd.saltel@gmail.com>
2023-05-12 23:54:34 +0200

    algo backtracking fonctionnel, remplacement des arguments par des pointers, fonction de maj des possibilités, comparaison des temps de l'algo avec/sans maj des possibilités

---
+++ utils.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math"
+)
+
+func IsOkayCase(x int, y int, grid [TAILLE + 2][TAILLE + 1]int, size int, value int) bool {
+	line := IsOkayLine(x, y, grid, size, value)
+	column := IsOkayColumn(x, y, grid, size, value)
+	box := IsOkayBox(x, y, grid, size, value)
+	return line && column && box
+}
+
+func IsOkayLine(x int, y int, grid [TAILLE + 2][TAILLE + 1]int, size int, value int) bool {
+	i := 0
+	for i < size && grid[x][i] != value {
+		i++
+	}
+	return i == size
+}
+
+func IsOkayColumn(x int, y int, grid [TAILLE + 2][TAILLE + 1]int, size int, value int) bool {
+	i := 0
+	for i < size && grid[i][y] != value {
+		i++
+	}
+	return i == size
+}
+
+func IsOkayBox(x int, y int, grid [TAILLE + 2][TAILLE + 1]int, size int, value int) bool {
+	root := int(math.Sqrt(float64(size)))
+	i := 0
+	for i < size && grid[int(x/root)*root+int(i/root)][int(y/root)*root+i%root] != value {
+		i++
+	}
+	return i == size
+}
